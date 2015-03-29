d64bbd2570e84a55c75c6c9ecd1e7dd32871e5a5
bibinoo <ze_bibinoo@yahoo.com>
2015-03-29 09:43:03 +0000

    Version without bug anymore however the function named check_arg_exists is not anymore used as not able to understand how to implement correctly the escape when an arg is not present or null

--- .bashfunction.lib.0.sh
+++ .bashfunction.lib.0.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #####################################################################
 # Functions
@@ -13,33 +13,30 @@ function check_arg_exists () {
 
 function check_dir_exists () {
 
-    # Check if arg is existing
-    echo "Before"
-    check_arg_exists $1
+    # If arg existing and greater than nothing then continue
+    if [[ "$#" != 0 ]] && [ ! -z "$1" ]; then
 
-    # If arg existing then continue
-    if [[ $? == 0 ]]; then
-	echo "in the loop"
 	# Setup local variable within function
 	local __arg1="$1"
     	
 	# Check if the dir and/or file is existing
 	if [ -d $__arg1 ]; then
 	    echo "$__arg1 is existing"
+	    return 0
    	else
 	    echo "$__arg1 is not existing"
 	    return 1
 	fi
+    else
+	echo "Parameter #1 is null or doesn't exist, aborting"
+	return 2
     fi
 }
 
 function check_file_exists () {
 
-    # Check if arg is existing
-    check_arg_exists $1
-
-    # If arg existing then continue
-    if test $? == 0; then
+    # If arg existing and greater than nothing then continue
+    if [[ "$#" != 0 ]] && [ ! -z "$1" ]; then
 
 	# Setup local variable within function
 	local __arg1="$1"
@@ -47,10 +44,14 @@ function check_file_exists () {
 	# Check if the dir and/or file is existing
 	if [ -f $__arg1 ]; then
 	    echo "$__arg1 is existing"
+	    return 0
    	else
 	    echo "$__arg1 is not existing"
 	    return 1
 	fi
+    else
+	echo "Parameter #1 is null or doesn't exist, aborting"
+	return 2
     fi
 }
 
@@ -68,6 +69,6 @@ function check_dir_exists_test () {
     check_dir_exists
 }
 
-#check_file_exists_test
+check_file_exists_test
 check_dir_exists_test
 #check_arg_exists
