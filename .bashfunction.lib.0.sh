09a5e1886c2ccf43acc767bcf59a254e677fefe7
bibinoo <ze_bibinoo@yahoo.com>
2015-03-29 09:01:43 +0000

    Initial version with 1 bug

--- .bashfunction.lib.0.sh
+++ .bashfunction.lib.0.sh
@@ -1,36 +1,73 @@
-#/bin/bash
+#!/bin/bash -x
 
 #####################################################################
 # Functions
 #####################################################################
 function check_arg_exists () {
-    if [ -z "$1" ]; then
+    if [[ "$#" -eq 0 ]] ; then
 	echo "Parameter #1 doesn't exist, aborting"
+	return 2
     fi
-    return $?
 }
 
 
 function check_dir_exists () {
-    local dir="$1"
-    if [ -d $dir ]; then
-	echo "$dir is existing"
-    else
-	echo "$dir is not existing"
-	return $?
+
+    # Check if arg is existing
+    echo "Before"
+    check_arg_exists $1
+
+    # If arg existing then continue
+    if [[ $? == 0 ]]; then
+	echo "in the loop"
+	# Setup local variable within function
+	local __arg1="$1"
+    	
+	# Check if the dir and/or file is existing
+	if [ -d $__arg1 ]; then
+	    echo "$__arg1 is existing"
+   	else
+	    echo "$__arg1 is not existing"
+	    return 1
+	fi
     fi
 }
 
 function check_file_exists () {
-    local __var1="$1"
-    if [ -f $__var1 ]; then
-	echo "$__var1 is existing"
-    else
-	echo "$__var1 is not existing"
-	return 1
+
+    # Check if arg is existing
+    check_arg_exists $1
+
+    # If arg existing then continue
+    if test $? == 0; then
+
+	# Setup local variable within function
+	local __arg1="$1"
+    	
+	# Check if the dir and/or file is existing
+	if [ -f $__arg1 ]; then
+	    echo "$__arg1 is existing"
+   	else
+	    echo "$__arg1 is not existing"
+	    return 1
+	fi
     fi
 }
-check_dir_exists ~/home-repo
-echo $?
-check_file_exists ~/home-repo/.profile
-echo $?
+
+function check_file_exists_test () {
+    check_file_exists ~/home-repo/.profile
+    check_file_exists ~/home-repo/.profile1
+    check_file_exists ""
+    check_file_exists
+}
+
+function check_dir_exists_test () {
+    check_dir_exists ~/home-repo
+    check_dir_exists ~/homerepo
+    check_dir_exists ""
+    check_dir_exists
+}
+
+#check_file_exists_test
+check_dir_exists_test
+#check_arg_exists
