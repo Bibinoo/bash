fa9b66cb4bdc7e17270adb435c4d2d42b1817f20
bibinoo <ze_bibinoo@yahoo.com>
2015-03-29 11:30:08 +0000

    New test library function

---
+++ .bashfunctiontest.lib.0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################################################
+# Functions to test
+#####################################################################
+function check_dir_exists_test () {
+    check_dir_exists ~/home-repo
+    check_dir_exists ~/homerepo
+    check_dir_exists ""
+    check_dir_exists
+}
+
+function check_file_exists_test () {
+    check_file_exists ~/home-repo/.profile
+    check_file_exists ~/home-repo/.profile1
+    check_file_exists ""
+    check_file_exists
+}
+
+function function_test () {
+check_file_exists_test
+check_dir_exists_test
+}
+
+function_test
